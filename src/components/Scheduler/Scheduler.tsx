2d7c6906f2893cfeab6dd67284048fa20e0452b5
Hououin Kyouma <inwazzin@gmail.com>
2023-02-16 23:09:15 +0100

    button

--- src/components/Scheduler/Scheduler.tsx
+++ src/components/Scheduler/Scheduler.tsx
@@ -3,7 +3,7 @@ import { SchedulerHeader } from '@/components/Scheduler/SchedulerHeader';
 import { SchedulerBody } from '@/components/Scheduler/SchedulerBody';
 
 export const Scheduler = () => (
-  <section className="flex flex-col gap-2 h-full w-full rounded bg-white px-2 py-4">
+  <section className="flex flex-col gap-2 h-full w-full rounded bg-white py-8 px-4">
     <SchedulerProvider>
       <SchedulerHeader />
       <SchedulerBody />
